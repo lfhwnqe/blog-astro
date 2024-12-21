a80d6683cacb3243a564e652a7ca071458b2f5a8
lfhwnqe <lfhwnqea@gmail.com>
2024-12-21 20:30:57 +0800

    feat: 增加 remark-mermaid 解析mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
+import remarkMermaid from 'remark-mermaidjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,11 +11,14 @@ export default defineConfig({
   site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
   trailingSlash: 'always', // Use to always append '/' at end of url
   markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'monokai',
-    },
+    // shikiConfig: {
+    //   // Choose from Shiki's built-in themes (or add your own)
+    //   // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+    //   theme: 'monokai',
+    // },
+    remarkPlugins: [remarkMermaid],
+
+    syntaxHighlight: false,
   },
   integrations: [react(), tailwind({}), sitemap(), robotsTxt()],
 });
